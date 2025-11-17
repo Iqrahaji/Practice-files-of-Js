388c8e560abaae602ad983bfcb7608cfd003a82a
Iqra haji <iqrahaji3946@gmail.com>
2025-11-17 15:19:35 +0500

    This is the second lesson.

---
+++ Course by Azad Chaiwala/JS files/Lesson no 2/script.js
@@ -0,0 +1,30 @@
+
+// var
+
+var name = "Iqra haji";
+console.log(name);
+
+// let
+let age = 20;
+console.log(age);
+
+// const
+const pi = 3.14;
+console.log(pi);
+
+var name = "Ali";
+console.log(name);
+// let age = 25; // This will throw an error
+// let age = 25;
+// console.log(age);
+
+// pi = 3.1415; // This will throw an error
+console.log(pi);    
+
+// Block scope with let and const
+if (true) {
+    let city = "Karachi";   
+    const country = "Pakistan";
+    console.log(city);
+    console.log(country);
+}
