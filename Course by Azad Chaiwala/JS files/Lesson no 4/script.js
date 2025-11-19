13be651e106cbea8d34d325463cbea643d48ea7b
Iqra haji <iqrahaji3946@gmail.com>
2025-11-19 16:51:20 +0500

    The last touch to the class 4.

--- Course by Azad Chaiwala/JS files/Lesson no 4/script.js
+++ Course by Azad Chaiwala/JS files/Lesson no 4/script.js
@@ -26,6 +26,25 @@ console.log(fruits); // Output: ["Strawberry", "Orange", "Mango"]
 //if we want to find the length of an array
 console.log(fruits.length); // Output: 3
 
+//if we want to sort an array
+fruits.sort();
+console.log(fruits); // Output: ["Mango", "Orange", "Strawberry"]
+
+//if we want to reverse an array
+fruits.reverse();
+console.log(fruits); // Output: ["Strawberry", "Orange", "Mango"]
+
+//if we want to find the index of a value in an array
+console.log(fruits.indexOf("Orange")); // Output: 1
+
+//if we want to check if a value is in an array
+console.log(fruits.includes("Mango")); // Output: true
+
+//if we want to join two arrays
+let vegetables = ["Carrot", "Potato"];
+let food = fruits.concat(vegetables);
+console.log(food); // Output: ["Strawberry", "Orange", "Mango", "Carrot", "Potato"]
+
 //objects in JavaScript
 let person = {
     firstName: "John",
@@ -44,4 +63,14 @@ console.log(person.city); // Output: New York
 //removing a property from the object
 delete person.lastName;
 console.log(person); // Output: {firstName: "John", age: 31, city: "New York"}
+
+
+//Parsing and stringifying the object.
+let personString = JSON.stringify(person);
+console.log(personString); // Output: "{"firstName":"John","age":31,"city":"New York"}" 
+
+let personObj = JSON.parse(personString);
+console.log(personObj); // Output: {firstName: "John", age: 31, city: "New York"}   
+
+
 
